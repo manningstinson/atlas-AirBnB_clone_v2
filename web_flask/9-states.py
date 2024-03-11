a01b98fdcc049b15f0383e803145090b3109bac9
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-03-11 17:36:05 +0000

    updated task 10

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -10,34 +10,28 @@ app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def teardown_db(exception):
-    """
-    Remove the current SQLAlchemy Session
-    """
+def teardown_session(exception):
+    """Closes the database session."""
     storage.close()
 
 
 @app.route('/states', strict_slashes=False)
 def states_list():
-    """
-    Display a HTML page with a list of all State objects present in DBStorage sorted by name (A->Z)
-    """
+    """Displays a list of all State objects."""
     states = storage.all("State").values()
-    sorted_states = sorted(states, key=lambda state: state.name)
-    return render_template('9-states.html', states=sorted_states)
+    states_sorted = sorted(states, key=lambda x: x.name)
+    return render_template('9-states.html', states=states_sorted)
 
 
 @app.route('/states/<id>', strict_slashes=False)
-def states_cities(id):
-    """
-    Display a HTML page with the list of City objects linked to the State sorted by name (A->Z)
-    """
+def state_cities(id):
+    """Displays cities of a specific State."""
     state = storage.get("State", id)
-    if state:
-        cities = sorted(state.cities, key=lambda city: city.name)
-        return render_template('9-states.html', state=state, cities=cities)
-    return render_template('9-states.html', not_found=True)
+    if state is None:
+        return render_template('9-states.html', not_found=True)
+    cities_sorted = sorted(state.cities, key=lambda x: x.name)
+    return render_template('9-states.html', state=state, cities=cities_sorted)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
